4f2c76428e7ada8915a467bbfc5c7f4b296d4c2c
SICONG DENG <sden2150@uni.sydney.edu.au>
2023-05-21 22:36:47 +1000

    4.html

--- public/script.js
+++ public/script.js
@@ -2,17 +2,17 @@
 const form = document.getElementById("taskform");
 const tasklist = document.getElementById("tasklist");
 
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  console.log(form.elements.taskType.value)
+  console.log(form.elements.taskTraining.value)
 
   addTask(
     form.elements.taskName.value,
-    form.elements.taskType.value,
-    form.elements.taskRate.value,
-    form.elements.taskTime.value,
-    form.elements.taskClient.value,
+    form.elements.taskTimes.value,
+    form.elements.taskWeight.value,
+    form.elements.taskTraining.value,
+    form.elements.taskDate.value,
   )
   console.log(taskList)
 })
@@ -20,7 +20,19 @@ form.addEventListener("submit", function(event) {
 function displayTask(task) {
   let item = document.createElement("li");
   item.setAttribute("data-id", task.id);
-  item.innerHTML = `<p><strong>${task.name}</strong><br>${task.type}</p>`;
+  item.innerHTML = `<p><strong>${task.name}</strong><br>${task.training}<br>${task.weight}KG X ${task.times}<br>${task.date}</p>`;
+
+  item.style.backgroundColor = "white";
+  item.style.width = "30%";
+  item.style.padding = "10px";
+  item.style.marginBottom = "10px";
+  item.style.borderRadius = "5px";
+  item.style.listStyle = "none";
+  // item.style.marginTop = "-10px";
+  item.style.marginLeft = "55%";
+  item.style.textAlign = "center";
+
+
 
   tasklist.appendChild(item);
 
@@ -33,10 +45,17 @@ function displayTask(task) {
   delButton.appendChild(delButtonText);
   item.appendChild(delButton); // Adds a delete button to every task
 
-  // Listen for when the delete button is clicked
-  delButton.addEventListener("click", function(event) {
+  delButton.style.backgroundColor = "#e74c3c";
+  delButton.style.color = "white";
+  delButton.style.border = "none";
+  delButton.style.padding = "5px 10px";
+  delButton.style.borderRadius = "5px";
+  delButton.style.cursor = "pointer";
 
-    taskList.forEach(function(taskArrayElement, taskArrayIndex) {
+
+  // Listen for when the delete button is clicked
+  delButton.addEventListener("click", function (event) {
+    taskList.forEach(function (taskArrayElement, taskArrayIndex) {
       if (taskArrayElement.id == item.getAttribute('data-id')) {
         taskList.splice(taskArrayIndex, 1)
       }
@@ -47,74 +66,29 @@ function displayTask(task) {
 
     item.remove(); // Remove the task item from the page when button clicked
     // Because we used 'let' to define the item, this will always delete the right element
-
   })
-
-  
 }
 
 
-
-
-// Create an object called 'task'
-// Populate the properties based on the provided data model
-
-// Commented out now the object creation is included in the function
-
-// var task = {
-//   name: "Initial Sketches",
-//   type: "Concept Ideation",
-//   id: Date.now(),
-//   date: new Date().toISOString(),
-//   rate: 50,
-//   time: 5,
-//   client: "Google"
-// }
-
-// console.log(task);
-
-
-// Create an array called 'taskList'
 var taskList = [];
 
-// Create a function called 'addTask'
-// Give the function input parameters for: name, type, rate, time, client
-// Paste your object definition from above in the function
-// Replace the property values with the input paramaters
-// Add the object to the taskList array
 
-function addTask(name, type, rate, time, client) {
-
-  // Creating the object with the usual property:value syntax
-  // Create task object 
-  // let task = {
-  //   name: name,
-  //   type: type,
-  //   id: Date.now(),
-  //   date: new Date().toISOString(),
-  //   rate: rate,
-  //   time: time,
-  //   client: client
-  // }
-
+function addTask(name, times, weight, training, date) {
   // Creating the object, directly passing in the input parameters
   let task = {
-    name,
-    type,
-    id: Date.now(),
-    date: new Date().toISOString(),
-    rate,
-    time,
-    client
+    name: name,
+    times: times,
+    weight: weight,
+    training: training,
+    date: date,
   }
 
   taskList.push(task);
   displayTask(task);
-
 }
 
 // Call the function with test values for the input paramaters
-addTask("Initial Sketches", "Concept Ideation", 50, 5, "Google");
+addTask("Initial Sketches", 5, 50, "Concept Ideation", "Google");
 
 // Log the array to the console.
 console.log(taskList);
